8b4f14e43daa53977544302470745395b6b966ca
sukebe828 <jacky828@gmail.com>
2025-07-28 01:30:39 -0700

    7

--- site.config.ts
+++ site.config.ts
@@ -20,8 +20,8 @@ export default siteConfig({
   twitter: '#',
   github: '#',
   linkedin: '#',
-  // mastodon: '#', // optional mastodon profile URL, provides link verification
-  // newsletter: '#', // optional newsletter URL
+  mastodon: '#', // optional mastodon profile URL, provides link verification
+  newsletter: '#', // optional newsletter URL
   youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
 
   // default notion icon and cover images for site-wide consistency (optional)
